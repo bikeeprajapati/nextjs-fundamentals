b3c076799cff80cd9b82a471c0852a3940bcdb89
Bikee Prajapati <bikeeprajapati1@gmail.com>
2026-07-20 16:02:04 +0545

    Enhance authentication layouts and add redirect pages for login, register, and forgot password

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -2,7 +2,7 @@ export default function ForgotPasswordPage() {
     return (
         <div>
             <h1>Forgot Password Page</h1>
-            <p>This is the forgot password page of the application.</p>
+            <p> hello there! This is the forgot password page of the application.</p>
         </div>
     );
 }
