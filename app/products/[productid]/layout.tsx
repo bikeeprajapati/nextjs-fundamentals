74e9263b61ea3ad7df2549e0929a4602f515a81d
Bikee Prajapati <bikeeprajapati1@gmail.com>
2026-07-21 11:42:44 +0545

    Add ErrorBoundary component for handling errors in product details layout

--- app/products/[productid]/layout.tsx
+++ app/products/[productid]/layout.tsx
@@ -1,8 +1,16 @@
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count);
+}
 export default function ProducyDetailsLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const random = getRandomInt(2);
+    if (random===1) {
+        throw new Error("Random error in product details layout");
+
+    }
     return (
         <div className="flex flex-col gap-4">
             <h1 className="text-2xl font-bold">Featured Products</h1>
