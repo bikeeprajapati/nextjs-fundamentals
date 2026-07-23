57bdda9989f2b59ec89093a86fa316b2156ae8ed
Bikee Prajapati <bikeeprajapati1@gmail.com>
2026-07-23 16:19:30 +0545

    Enhance GET method in profile API to include cookie handling and set default results per page

--- route-handlers-demo/app/profile/api/route.ts
+++ route-handlers-demo/app/profile/api/route.ts
@@ -1,6 +1,13 @@
 import { NextRequest } from 'next/dist/server/web/spec-extension/request';
+import { headers, cookies } from 'next/headers';
 export async function GET(request: NextRequest) {
     const requestHeaders = new Headers(request.headers);
     console.log(requestHeaders.get('Authorization'));
-    return new Response("<h1>Profile API Data</h1>",{ headers: { "Content-Type": "text/html" } });
+    const requestCookies = request.cookies;
+    console.log(requestCookies.get('theme'));
+    const cookiesStore =  await cookies();
+    console.log(cookiesStore.get('theme'));
+    cookiesStore.set('resultsPerPage','20');
+    return new Response("<h1>Profile API Data</h1>",{ headers: { "Content-Type": "text/html" ,
+    "Set-Cookie":"theme=dark;" }});
 }
