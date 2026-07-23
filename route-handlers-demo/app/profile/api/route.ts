04b6ab6568efded7d20570e57b7c948f8441f709
Bikee Prajapati <bikeeprajapati1@gmail.com>
2026-07-23 15:51:18 +0545

    Refactor GET method in profile API to include request handling and authorization logging

--- route-handlers-demo/app/profile/api/route.ts
+++ route-handlers-demo/app/profile/api/route.ts
@@ -1,3 +1,6 @@
-export async function GET() {
-    return new Response("Profile API Data");
+import { NextRequest } from 'next/dist/server/web/spec-extension/request';
+export async function GET(request: NextRequest) {
+    const requestHeaders = new Headers(request.headers);
+    console.log(requestHeaders.get('Authorization'));
+    return new Response("<h1>Profile API Data</h1>",{ headers: { "Content-Type": "text/html" } });
 }
