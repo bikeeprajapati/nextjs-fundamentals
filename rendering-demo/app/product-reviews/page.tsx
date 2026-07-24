bcf33d402be6da7304bd9a119f7361ab9d2f8984
Bikee Prajapati <bikeeprajapati1@gmail.com>
2026-07-24 12:42:01 +0545

    feat: add Product and Reviews components with async rendering to ProductReviews page

---
+++ rendering-demo/app/product-reviews/page.tsx
@@ -0,0 +1,18 @@
+import { Suspense } from "react";
+import { Product } from "@/app/components/product";
+import { Reviews } from "@/app/components/reviews";
+
+export default function ProductReviews() {
+    return (
+        <div>
+            <h1>Product Reviews Page</h1>
+            <Suspense fallback={<div>Loading Product...</div>}>
+                <Product />
+            </Suspense>
+            <Suspense fallback={<div>Loading Reviews...</div>}>
+                <Reviews />
+            </Suspense>
+
+        </div>
+    );
+}
