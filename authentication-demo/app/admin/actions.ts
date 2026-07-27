7c4c54aeb4f667935332de16af23d05b2d5c19cd
Bikee Prajapati <bikeeprajapati1@gmail.com>
2026-07-27 12:59:32 +0545

    Fix incorrect relative import path in admin actions

--- authentication-demo/app/admin/actions.ts
+++ authentication-demo/app/admin/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 import { auth } from "@clerk/nextjs/server";
 import { clerkClient } from "@clerk/nextjs/server";
-import { Roles } from "../../../types/globals";
+import { Roles } from "@/types/globals";
 import { revalidatePath } from "next/cache";
 
 export async function setRole(formData: FormData) {
